4e44ef9b4818252aad05b0072867f9e7e8964edf
TJ Horner <rowenrocks@gmail.com>
2014-11-06 17:12:48 -0800

    FIXED

---
+++ Main.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SMART_Table_Activty_Exporter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.exportButton = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.transferActivites = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(13, 13);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(339, 23);
+            this.exportButton.TabIndex = 0;
+            this.exportButton.Text = "Export a SMART Table Activity";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // importButton
+            // 
+            this.importButton.Location = new System.Drawing.Point(13, 42);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(339, 23);
+            this.importButton.TabIndex = 1;
+            this.importButton.Text = "Import an activity to a Toolkit file";
+            this.importButton.UseVisualStyleBackColor = true;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // transferActivites
+            // 
+            this.transferActivites.Location = new System.Drawing.Point(12, 71);
+            this.transferActivites.Name = "transferActivites";
+            this.transferActivites.Size = new System.Drawing.Size(339, 23);
+            this.transferActivites.TabIndex = 2;
+            this.transferActivites.Text = "Transfer activities between projects directly";
+            this.transferActivites.UseVisualStyleBackColor = true;
+            this.transferActivites.Click += new System.EventHandler(this.transferActivites_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 108);
+            this.Controls.Add(this.transferActivites);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.exportButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "SMART Table Activity Importer/Exporter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Button importButton;
+        private System.Windows.Forms.Button transferActivites;
+    }
+}
---
+++ TableTools.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+using Ionic.Zip;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SMART_Table_Activty_Exporter
+{
+    class TableTools
+    {
+        public static string GetManifestText(string fpath)
+        {
+            System.IO.Compression.ZipArchive contentFile = System.IO.Compression.ZipFile.OpenRead(fpath);
+            System.IO.Compression.ZipArchiveEntry manifest = contentFile.GetEntry("imsmanifest.xml");
+            TextReader textReader = new StreamReader(manifest.Open());
+            return textReader.ReadToEnd();
+        }
+
+        public static void ExtractActivityDirectory(string activity, string fpath, string opath)
+        {
+            using (ZipFile contentFile = ZipFile.Read(fpath))
+            {
+                var selection = (from e in contentFile.Entries
+                                 where (e.FileName).StartsWith(activity+"/")
+                                 select e);
+
+                Directory.CreateDirectory(opath);
+                foreach (var e in selection)
+                {
+                    e.Extract(opath);
+                }
+            }
+        }
+
+        public static List<string> GetActivitiesFromManifest(string text)
+        {
+            List<string> activities = new List<string>();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(text);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            JObject manifest = (JObject) JObject.Parse(jsonText)["manifest"];
+            JObject orgs = (JObject) manifest["organizations"];
+            for (int i = 0; i < orgs.Count; i++)
+            {
+                JObject org = (JObject) orgs["organization"];
+                if(org["item"].GetType().ToString().Equals("Newtonsoft.Json.Linq.JArray")){
+                    JArray items = (JArray) org["item"];
+                    for (int i2 = 0; i2 < items.Count; i2++)
+                    {
+                        JObject item = (JObject) items[i2];
+                        activities.Add((string) item["title"]);
+                    }
+                }else{
+                    JObject item = (JObject) org["item"];
+                    activities.Add((string) item["title"]);
+                }
+            }
+            return activities;
+        }
+
+        public static string GetResourceXmlForActivity(string text, string name){
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(text);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            JObject manifest = (JObject)JObject.Parse(jsonText)["manifest"];
+            return "";
+        }
+
+        public static string GetActivityName(string fpath)
+        {
+            string config = File.ReadAllText(Directory.GetFiles(fpath, "config.xml")[0]);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(config);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            JObject configObj = (JObject) JObject.Parse(jsonText)["TableApplication"];
+            return (string) configObj["@name"];
+        }
+    }
+}
